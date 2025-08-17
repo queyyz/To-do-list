372db0ed12d5679f49b7fa314a28dbaf8caa8c25
que <melovekamisatoayaka@gmail.com>
2025-08-17 23:11:36 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+// src/App.jsx
 import React from 'react';
 import TodoAppContainer from './containers/TodoAppContainer';
 import './App.css';
 
 function App() {
+  // 🔽 ไม่มี state ไม่มี logic มีแค่การเรียกใช้ Container
   return <TodoAppContainer />;
 }
 
